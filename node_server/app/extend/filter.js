84e56ccc74dfb8d2b49325515aacd935a8cfc69c
jskhds <2987163443a@gmail.com>
2021-10-11 13:49:11 +0800

    first commit

---
+++ node_server/app/extend/filter.js
@@ -0,0 +1,10 @@
+// 年月日的过滤器
+exports.dateformate = time => {
+    let date = new Date(time);
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let result = `${year}年${month}月${day}日`;
+    return result
+};
+
